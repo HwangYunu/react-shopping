4b53cc9492c987e18f0f493b9864aa95f05df337
HwangYunu <lybear108@gmail.com>
2022-10-02 22:07:54 +0900

    라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import { useState } from 'react';
-import { Row, Col, Navbar, Container, Nav } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   let [bears] = useState(data);
 
   return (
     <div className="App">
-      <Navbar
-        bg="light"
-        variant="light">
-        <Container>
-          <Navbar.Brand href="#home">BearShop</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <ul>
+        <li>
+          <Link to="/">BearShop</Link>
+        </li>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/detail">상세페이지</Link>
+        </li>
+      </ul>
 
-      <div className="main-bg"></div>
-
-      <ProductComponent bears={bears} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <ProductComponent bears={bears} />
+            </>
+          }
+        />
+        <Route
+          path="/detail"
+          element={<div>상세페이지임</div>}
+        />
+      </Routes>
     </div>
   );
 }
