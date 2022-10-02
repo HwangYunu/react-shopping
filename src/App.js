ab4b1e04c5e859eebc5d5b12f5c57a5c276d16b0
HwangYunu <lybear108@gmail.com>
2022-10-02 17:00:43 +0900

    기본화면 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
+import { Row, Col, Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar
+        bg="light"
+        variant="light">
+        <Container>
+          <Navbar.Brand href="#home">BBshop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      <Container>
+        <Row>
+          <Col sm>
+            <img
+              src="http://m.imaxtore.com/web/product/extra/big/201808/697aa961184b01ef6cab13246c9e4c06.jpg"
+              width="80%"
+              alt=""
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+
+          <Col sm>
+            <img
+              src="http://m.imaxtore.com/web/product/extra/big/201808/360d07d224bbedefd49a1277a998de66.jpg"
+              width="80%"
+              alt=""
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col sm>
+            <img
+              src="http://m.imaxtore.com/web/product/extra/big/201808/ae8e01ed60c4794403548f89bb1f0039.jpg"
+              width="80%"
+              alt=""
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
